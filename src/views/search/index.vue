96cb82a6f7ab23478a06d13029e05789bfc91966
Xuedixuedi <958190137@qq.com>
2021-05-31 23:56:57 +0800

    finish get img list and show

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -11,10 +11,34 @@
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
         </el-upload>
         <el-button @click="search()">Test</el-button>
-        <!-- <el-form>
-            <el-input type="file" v-model="file" required></el-input>
-        </el-form>
-        <el-button @click="search()">Test</el-button> -->
+        <div>
+            <el-row>
+                <el-col
+                    :span="5"
+                    v-for="(o, index) in this.picPathList"
+                    :key="index"
+                    :offset="2"
+                >
+                    <el-card>
+                        <img
+                            :src="o.imgUrl"
+                            class="image"
+                            width="200"
+                            height="200"
+                        />
+                        <div style="padding: 14px;">
+                            <span>{{ o.tag }}</span>
+                            <div class="bottom clearfix">
+                                <el-button type="text" class="button">
+                                    Like
+                                </el-button>
+                            </div>
+                        </div>
+                    </el-card>
+                </el-col>
+            </el-row>
+            <el-button @click="show()">haha</el-button>
+        </div>
     </div>
 </template>
 
@@ -22,14 +46,20 @@
 import { mapGetters } from "vuex"
 import axios from "axios"
 var baseUrl = "http://localhost:5000"
+//存放结果图片的地址
 
 export default {
     name: "Search",
     data() {
         return {
+            imageUrl: "",
             formData: null,
             file: null,
-            imageUrl: ""
+            picList: [],
+            picTagList: [], //与pic对应关系
+            tagList: [],
+            likeList: [],
+            picPathList: []
         }
     },
     methods: {
@@ -37,25 +67,110 @@ export default {
             console.log(item.file)
             this.file = item.file
         },
-        search() {
+        setPath() {
+            this.picPathList = []
+
+            console.log(this.picPathList)
+            this.picPathList = [
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[0]),
+                    name: this.picList[0],
+                    tag: this.picTagList[0].split(".")[0]
+                },
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[1]),
+                    name: this.picList[1],
+                    tag: this.picTagList[1].split(".")[0]
+                },
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[2]),
+                    name: this.picList[2],
+                    tag: this.picTagList[2].split(".")[0]
+                },
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[3]),
+                    name: this.picList[3],
+                    tag: this.picTagList[3].split(".")[0]
+                },
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[4]),
+                    name: this.picList[4],
+                    tag: this.picTagList[4].split(".")[0]
+                },
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[5]),
+                    name: this.picList[5],
+                    tag: this.picTagList[5].split(".")[0]
+                },
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[6]),
+                    name: this.picList[6],
+                    tag: this.picTagList[6].split(".")[0]
+                },
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[7]),
+                    name: this.picList[7],
+                    tag: this.picTagList[7].split(".")[0]
+                },
+                {
+                    imgUrl: require("@/../../lab2-image-retrieval/server/static" +
+                        this.picList[8]),
+                    name: this.picList[8],
+                    tag: this.picTagList[8].split(".")[0]
+                }
+            ]
+            console.log(this.picPathList)
+        },
+        async search() {
             this.formData = new FormData()
             this.formData.append("file", this.file)
-            console.log("file", this.file)
-            console.log("formData", this.formData)
 
-            axios
+            const res = await axios
                 .post(baseUrl + "/imgUpload", this.formData)
-                .then(res => {
-                    console.log("formData", this.formData)
-                    console.log("res:", res)
-                })
-
                 .catch(err => {
                     this.$message({
                         message: err.message,
                         type: "error"
                     })
                 })
+
+            console.log("res.data:", res.data)
+
+            this.tagList = res.data.tagList
+
+            this.picList.push(res.data.image0[0])
+            this.picTagList.push(res.data.image0[1])
+            this.picList.push(res.data.image1[0])
+            this.picTagList.push(res.data.image1[1])
+            this.picList.push(res.data.image2[0])
+            this.picTagList.push(res.data.image2[1])
+            this.picList.push(res.data.image3[0])
+            this.picTagList.push(res.data.image3[1])
+            this.picList.push(res.data.image4[0])
+            this.picTagList.push(res.data.image4[1])
+            this.picList.push(res.data.image5[0])
+            this.picTagList.push(res.data.image5[1])
+            this.picList.push(res.data.image6[0])
+            this.picTagList.push(res.data.image6[1])
+            this.picList.push(res.data.image7[0])
+            this.picTagList.push(res.data.image7[1])
+            this.picList.push(res.data.image8[0])
+            this.picTagList.push(res.data.image8[1])
+
+            // this.setPath()
+        },
+        show() {
+            console.log("tagList:", this.tagList)
+            console.log("picList:", this.picList)
+            this.setPath()
         }
     }
 }
@@ -93,5 +208,34 @@ export default {
     width: 178px;
     height: 178px;
     display: block;
+}
+.time {
+    font-size: 13px;
+    color: #999;
+}
+
+.bottom {
+    margin-top: 13px;
+    line-height: 12px;
+}
+
+.button {
+    padding: 0;
+    float: right;
+}
+
+.image {
+    width: 100%;
+    display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+    display: table;
+    content: "";
+}
+
+.clearfix:after {
+    clear: both;
 }
 </style>
